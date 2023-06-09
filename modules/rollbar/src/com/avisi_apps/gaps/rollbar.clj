247fccf17ede52c1fbb7a4ad3ae5938c00faf5cb
Jannick Joosten <j.joosten@avisi.nl>
2023-06-09 16:41:21 +0200

    Explicitly bind comp/*parent* in BodyContainer
    By `with-parent-context` doesn't actually set the parent passed in to
    the parent, but it uses the containing component. So every element got
    the BodyContainer as it's parent. By explicitly binding comp/*parent* we
    bypass that behaviour, but keeping the component offset for the ErrorBoundary.

--- modules/rollbar/src/com/avisi_apps/gaps/rollbar.clj
+++ modules/rollbar/src/com/avisi_apps/gaps/rollbar.clj
@@ -9,10 +9,13 @@
     * :error-message, the message that will be sent along the error
     * :fallback-ui-fn, the UI components to be rendered when an error occurs
 
+  The `:error-message` can also be a function with the signature: `(fn [error props] message-to-sent)`,
+  see Rollbar's react documentation for more details on the props.
+
   The `:fallback-ui-fn` function takes function with the signature: `(fn [props] what-to-render)`,
   where the props is a bean with the key `:error`."
   [opts & body]
   `(error-boundary*
      (assoc ~opts
        :parent comp/*parent*
-       :render #(comp/fragment ~@body))))
+       :render #(comp/with-parent-context % (comp/fragment ~@body)))))
